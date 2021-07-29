7b479723c7a06e432f02e5cc9e8a8f546b9cafaa
Nikita <myk.holitsyn@gmail.com>
2021-07-29 23:24:39 +0300

    js

--- JS-full/lesson4/task7/index.js
+++ JS-full/lesson4/task7/index.js
@@ -1,14 +1,14 @@
 let sum = 0;
 
 for (let i = 0; i <= 1000; i += 1) {
-  if (i % 2 === 0) {
-    continue;
+  if (i % 2 !== 0) {
+    sum += i;
+    console.log('Found');
   }
-   sum += i;
-   console.log('Found');
-   if (sum * 5 > 5000) {
-     console.log('Bigger');
-   } else {
-     console.log('Smaller or equal');
-   }
+}
+
+if (sum * 5 > 5000) {
+  console.log('Bigger');
+} else {
+  console.log('Smaller or equal');
 }
