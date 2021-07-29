2db4ab8ca9a3bbaef41e409e4a4b4987e806679b
Nikita <myk.holitsyn@gmail.com>
2021-07-29 22:37:11 +0300

    js

---
+++ JS-full/lesson3/task4/index.js
@@ -0,0 +1,26 @@
+console.log("false || true:", false || true);
+console.log("false || true || false:", false || true || false);
+console.log("false || false || false:", false || false || false);
+console.log("false && true:", false && true);
+console.log("false && true && false:", false && true && false);
+console.log("true && true && true:", true && true && true);
+console.log("'text' || false:", "text" || false);
+console.log("true || 'text':", true || "text");
+console.log(
+  "undefined || null || 0 || '' || null:",
+  undefined || null || 0 || "" || null
+);
+const customAmount = null;
+const defaultAmount = 17;
+const amount = customAmount || defaultAmount;
+console.log("amount:", amount);
+console.log("'text' && false:", "text" && false);
+console.log("true && 'text':", true && "text");
+console.log(
+  "undefined && null && 0 && '' && null:",
+  undefined && null && 0 && "" && null
+);
+console.log("!true:", !true);
+console.log("!false:", !false);
+console.log("!'text':", !"text");
+console.log("!!'text':", !!"text");
---
+++ JS-full/lesson4/task1/index.js
@@ -0,0 +1,7 @@
+const n = 125;
+let sum = 0;
+
+for (let i = 1; i <= n; i += 1) {
+  sum += i;
+}
+console.log('Result:', sum);
