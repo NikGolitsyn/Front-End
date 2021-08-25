6a373133a323c56dfb37b8f42118e36e98f869a6
Nikita <myk.holitsyn@gmail.com>
2021-08-25 15:07:37 +0300

    all

---
+++ JS-full/lesson21/hw3/index.js
@@ -0,0 +1,15 @@
+export function getTitle() {
+  return document.querySelector('.title').textContent;
+}
+
+export function getDescription() {
+  return document.querySelector('.about').innerText;
+}
+
+export function getPlans() {
+  return document.querySelector('.plans').innerHTML;
+}
+
+export function getGoal() {
+  return document.querySelector('.goal').outerHTML;
+}
