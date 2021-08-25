81bea5e1059484b48f6a29501fc0bdf91d9140c0
NikGolitsyn <78685778+NikGolitsyn@users.noreply.github.com>
2021-08-25 15:11:03 +0300

    Delete JS-full/lesson21/hw2 directory

--- JS-full/lesson21/hw2/index.js
+++
@@ -1,9 +0,0 @@
-export function getItemsList() {
-  console.dir(document.querySelectorAll('.technology'));
-  return document.querySelectorAll('.technology');
-}
-
-export function getItemsArray() {
-  console.dir(Array.from(document.querySelectorAll('.tool')));
-  return Array.from(document.querySelectorAll('.tool'));
-}
