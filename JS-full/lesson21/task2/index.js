760fd5813fbd5f2ee6072b3ad53096145fb6880f
Nikita <myk.holitsyn@gmail.com>
2021-08-25 14:43:45 +0300

    all

---
+++ JS-full/lesson21/task2/index.js
@@ -0,0 +1,9 @@
+export function getTitleElement() {
+  console.dir(document.querySelector('.title'));
+  return document.querySelector('.title');
+}
+
+export function getInputElement() {
+  console.dir(document.querySelector('input[type=text]'));
+  return document.querySelector('input[type=text]');
+}
