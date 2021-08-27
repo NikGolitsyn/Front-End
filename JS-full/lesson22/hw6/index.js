3b885953ceeec2c11517968abb7122ad88765334
Nikita <myk.holitsyn@gmail.com>
2021-08-27 20:10:54 +0300

    all

--- JS-full/lesson22/hw6/index.js
+++ JS-full/lesson22/hw6/index.js
@@ -1,7 +1,7 @@
 const btnJs = document.querySelector('.btn');
 const btnHtml = document.querySelector('.btn').nextElementSibling;
 
-const handleClick = (event) => {
+const handleClick = event => {
   console.log(event.target.textContent);
 };
 
