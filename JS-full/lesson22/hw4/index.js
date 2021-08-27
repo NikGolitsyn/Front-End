b4b23a0cc094bfa35e59d133059052b6e6196bab
Nikita <myk.holitsyn@gmail.com>
2021-08-27 19:27:14 +0300

    all

---
+++ JS-full/lesson22/hw4/index.js
@@ -0,0 +1,6 @@
+const searchButton = document.querySelector('.search__btn');
+const inputText = document.querySelector('.search__input');
+
+searchButton.addEventListener('click', () => {
+  console.log(inputText.value);
+});
