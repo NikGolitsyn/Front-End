b1b2ff6dcb7fa42e1153245f59dd4c0bd82582b4
Nikita <myk.holitsyn@gmail.com>
2021-09-01 01:34:15 +0300

    all

--- JS-full/lesson24/hw1/index.js
+++ JS-full/lesson24/hw1/index.js
@@ -3,18 +3,16 @@
 
 // algo
 // find amount of days
-// find amountt of hours, minutes, seconds
-// use Date .get and .set methods
+// find amount of hours, minutes, seconds
+// with the help of reminder % we can find the remaining of seconds, minutes, hours
 
 export function getDiff(startDate, endDate) {
-  const time = startDate.getTime() - endDate.getTime();
+  const seconds = Math.abs(startDate.getTime() - endDate.getTime()) / 1000;
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
 
-  const days = Math.abs(Math.round(time / (1000 * 3600 * 24)));
-  const hours = Math.abs(startDate.getHours() - endDate.getHours());
-  const minutes = Math.abs(startDate.getMinutes() - endDate.getMinutes());
-  const seconds = Math.abs(startDate.getSeconds() - endDate.getSeconds());
-
-  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  return `${days}d ${hours % days}h ${minutes % hours}m ${seconds % minutes}s`;
 }
 
 const test1 = new Date(2011, 1, 2, 13, 32, 47);
@@ -24,4 +22,3 @@ const test2 = new Date(2001, 1, 2, 15, 34, 25);
 console.log(getDiff(test1, test2));
 console.log(getDiff(new Date(2001, 10, 32, 3, 12, 27), new Date(2003, 1, 2, 15, 56, 45)));
 console.log(getDiff(new Date(1971, 3, 22, 13, 32, 47), new Date(1971, 7, 39, 2, 11, 15)));
-
