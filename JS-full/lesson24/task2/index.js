a619ca89c60b2b6d6c8fabe791a513d18afb897d
Nikita <myk.holitsyn@gmail.com>
2021-08-31 16:46:47 +0300

    all

--- JS-full/lesson24/task2/index.js
+++ JS-full/lesson24/task2/index.js
@@ -1,10 +1,11 @@
 // input data
 // output sting with GTM time in format '06:30'
 
-export const getGreenwichTime = date =>
+export function getGreenwichTime(date) {
   new Intl.DateTimeFormat('en', {
     timeZone: 'GMT',
     hour12: false,
     hour: '2-digit',
     minute: '2-digit',
   }).format(date);
+}
