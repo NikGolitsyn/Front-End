6ecb05f409918e0b8a74df247e87ea50f59ad3f0
NikGolitsyn <78685778+NikGolitsyn@users.noreply.github.com>
2021-09-01 13:12:13 +0300

    Update index.js

--- JS-full/lesson24/hw1/index.js
+++ JS-full/lesson24/hw1/index.js
@@ -13,6 +13,7 @@ export function getDiff(startDate, endDate) {
   const minutes = Math.trunc(time / (60 * 1000));
   const hours = Math.trunc(time / (60 * 60 * 1000));
   const days = Math.trunc(time / (24 * 3600 * 1000));
+
   function calc(firstElem, secondElem) {
     const result = firstElem % secondElem;
     if (isNaN(result)) {
@@ -25,78 +26,34 @@ export function getDiff(startDate, endDate) {
 }
 
 
-// console.log(getDiffTest(new Date(2011, 1, 2, 13, 32, 47), new Date(2001, 1, 2, 15, 34, 25)));
+
 // console.log(getDiff(new Date(2011, 1, 2, 13, 32, 47), new Date(2001, 1, 2, 15, 34, 25)));
-
-// console.log(getDiffTest(new Date(2001, 10, 32, 3, 12, 27), new Date(2003, 1, 2, 15, 56, 45)));
 // console.log(getDiff(new Date(2001, 10, 32, 3, 12, 27), new Date(2003, 1, 2, 15, 56, 45)));
-
-// console.log(getDiffTest(new Date(1971, 3, 22, 13, 32, 47), new Date(1971, 7, 39, 2, 11, 15)));
 // console.log(getDiff(new Date(1971, 3, 22, 13, 32, 47), new Date(1971, 7, 39, 2, 11, 15)));
-
-// console.log(
-//   'equal ',
-//   getDiffTest(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 1, 2, 13, 32, 47)),
-// );
 // console.log('equal ', getDiff(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 1, 2, 13, 32, 47)));
-
-// console.log(
-//   'seconds test ',
-//   getDiffTest(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 1, 2, 13, 32, 48)),
-// );
 // console.log(
 //   'seconds test ',
 //   getDiff(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 1, 2, 13, 32, 48)),
-// );
-
-// console.log(
-//   'minutes test ',
-//   getDiffTest(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 1, 2, 13, 33, 47)),
 // );
 // console.log(
 //   'minutes test ',
 //   getDiff(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 1, 2, 13, 33, 47)),
 // );
-
-// console.log(
-//   'hours test ',
-//   getDiffTest(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 1, 2, 14, 32, 47)),
-// );
 // console.log(
 //   'hours test ',
 //   getDiff(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 1, 2, 14, 32, 47)),
-// );
-
-// console.log(
-//   'days test ',
-//   getDiffTest(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 1, 3, 13, 32, 47)),
 // );
 // console.log(
 //   'days test ',
 //   getDiff(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 1, 3, 13, 32, 47)),
 // );
-
-// console.log(
-//   'month test ',
-//   getDiffTest(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 2, 2, 13, 32, 47)),
-// );
 // console.log(
 //   'month test ',
 //   getDiff(new Date(2011, 1, 2, 13, 32, 47), new Date(2011, 2, 2, 13, 32, 47)),
 // );
-
-// console.log(
-//   'years test',
-//   getDiffTest(new Date(2011, 1, 2, 13, 32, 47), new Date(2012, 1, 2, 13, 32, 47)),
-// );
 // console.log(
 //   'years test',
 //   getDiff(new Date(2011, 1, 2, 13, 32, 47), new Date(2012, 1, 2, 13, 32, 47)),
-// );
-
-// console.log(
-//   'years test',
-//   getDiffTest(new Date(2011, 1, 2, 13, 32, 47), new Date(2015, 1, 2, 13, 32, 47)),
 // );
 // console.log(
 //   'years test',
