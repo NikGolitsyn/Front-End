2ae38a2b8974f8da7540d7604bd38e5b10450a7a
Nikita <myk.holitsyn@gmail.com>
2021-09-03 18:12:57 +0300

    all

---
+++ JS-full/lesson27/task1/index.js
@@ -0,0 +1,61 @@
+localStorage.clear(); // удаляет все значения из хранилища / input nine / output none
+localStorage.setItem('hobbies', JSON.stringify({ name: 'Tomas' }));
+// setItem задаёт или обновляет значение по ключу
+// input 'name of key' 'value' / output none
+localStorage.setItem('lastName', 'Anderson');
+localStorage.setItem('age', JSON.stringify(17));
+
+// console.log(JSON.parsa(localStorage.getItem('hobbies')));
+// localStorage свойство которое позволяет получить доступ к объекту Storage.
+// Storage предоставляет доступ для ssession storage или local storage для конкретного домена,
+// позволяя добавлять, изменять, удалять сохраненные элементы данных.
+
+// input none 
+// output object
+
+// function getLocalStorageData() {
+//   console.log(localStorage);
+//   console.log(Object.entries(localStorage)); // создаем массив подмассивов ключей и значений
+//   return Object.entries(localStorage).reduce((acc, [key, value]) => {
+//     // методом редьюс собираем объект, приминяем деструкторизацию к каждому подмассиву в массиве entries
+//     console.log([key, value]);
+//     let newValue; // объявляем новое значение которое будет использовать при проверке try/ catch
+//     try {
+//       // Конструкция try...catch пытается выполнить инструкции в блоке try, и, в случае ошибки, выполняет блок catch.
+//       newValue = JSON.parse(value); // попытка строчное значение в другое с помощью JSON.parse()
+//       console.log(newValue);
+//     } catch (e) {
+//       //  Блок catch задаёт идентификатор (e) который содержит объект исключения
+//       console.log(e);
+//       newValue = value;
+//     }
+//     console.log(acc);
+//     return {
+//       // добавляем к копии объекта acc ключи и значения, слияние с помощью спреад
+//       ...acc,
+//       [key]: newValue,
+//     };
+//   }, {});
+// }
+
+localStorage.setItem('married', false);
+localStorage.setItem('amount of children', null);
+
+// input none
+// output object
+
+export function getLocalStorageData() {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
+    return {
+      ...acc,
+      [key]: newValue,
+    };
+  }, {});
+}
+console.log(getLocalStorageData());
--- JS-full/lesson9/task11/index.js
+++ JS-full/lesson9/task11/index.js
@@ -1,9 +1,15 @@
-const getCustomersList = obj => {
-  const order = Object.entries(obj).sort((a, b) => a[1].age - b[1].age);
-  const first = order.map(el => el[1]);
-  const second = order.map(el => el[0]);
-  return first.map((el, index) => ({ ...el, id: second[index] }));
-};
+// input object
+// output array
+
+// Algo
+// 1 Create an array of key values Object.entries
+// 2 Sort an array
+// 3 Use map to create a new array of objects with correct id
+
+// const getCustomersList = obj =>
+//   Object.entries(obj)
+//     .sort((a, b) => a[1].age - b[1].age)
+//     .map(el => ({ ...el[1], id: el[0] }));
 
 const customers = {
   'customers-id-1': {
@@ -23,3 +29,28 @@ const customers = {
     age: 25,
   },
 };
+
+const getCustomersList = obj => {
+  const order = Object.entries(obj).sort((a, b) => a[1].age - b[1].age);
+
+  const first = order.map(el => el[1]);
+  console.log(first);
+  const second = order.map(el => el[0]);
+
+  console.log(order.map(el => ({ ...el[1], id: el[0] })));
+  return first.map((el, index) => ({ ...el, id: second[index] }));
+};
+
+
+
+
+// const getCustomersList = obj => {
+//   const order = Object.entries(obj).sort((a, b) => a[1].age - b[1].age);
+//   console.log(order);
+//   const first = order.map(el => el[1]);
+//   console.log(first);
+//   const second = order.map(el => el[0]);
+//   console.log(second);
+//   console.log(order.map(el => ({ ...el[1], id: el[0] })));
+//   return first.map((el, index) => ({ ...el, id: second[index] }));
+// };
