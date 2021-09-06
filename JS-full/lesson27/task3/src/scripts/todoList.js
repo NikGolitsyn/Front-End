db276e81d25f22f77ec408ab2903d5de441ce7bb
Nikita <myk.holitsyn@gmail.com>
2021-09-06 13:35:11 +0300

    comments

--- JS-full/lesson27/task3/src/scripts/todoList.js
+++ JS-full/lesson27/task3/src/scripts/todoList.js
@@ -2,9 +2,9 @@ import { createTask } from './createTask.js';
 import { toggleTask } from './updateTasks.js';
 
 export const initTodoListHandler = () => {
-  const createBtnElem = document.querySelector('.create-task-btn');
-  createBtnElem.addEventListener('click', createTask);
+  const createBtnElem = document.querySelector('.create-task-btn'); // выбираем кнопку создать с помощью DOM
+  createBtnElem.addEventListener('click', createTask); // при клике вызываем ф-ю создать задание
 
-  const todoListElem = document.querySelector('.list');
-  todoListElem.addEventListener('click', toggleTask);
+  const todoListElem = document.querySelector('.list'); // выбираем список ul
+  todoListElem.addEventListener('click', toggleTask);  // при клике на чекбокс меняем статус done
 };
