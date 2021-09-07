a2bc596daff7aeeb0c2af769e4ee1f93ada3edf8
Nikita <myk.holitsyn@gmail.com>
2021-09-07 15:07:25 +0300

    all

---
+++ JS-full/lesson30/task1/index.js
@@ -0,0 +1,50 @@
+// input imgage source, callback function
+// output none
+
+// algo
+// create promise
+// 1 create an img element
+// 2 set source and add it to our page
+// 3 add eventListener function after load 
+// 4 add eventListener in case of 'error'
+
+export const addImageWithPromise = imgSrc => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+
+    imgElem.addEventListener('error', () => reject(new Error('Image load is failed')));
+  });
+  return p;
+};
+
+const imgSrc =
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+const resultPromise = addImageWithPromise(imgSrc);
+resultPromise.then(data => console.log(data));
+resultPromise.catch(error => console.log(error));
+
+// examples
+// addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//   onImageLoaded,
+// );
+// addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596',
+//   onImageLoaded,
+// );
+// addImage(
+//   '',
+//   onImageLoaded,
+// );
