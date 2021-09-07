b463b9254c12d4a30f284073327139184911f18e
Nikita <myk.holitsyn@gmail.com>
2021-09-07 08:34:21 +0300

    all

---
+++ JS-full/lesson29/task1/index.js
@@ -0,0 +1,44 @@
+// input imgage source, callback function
+// output none
+
+// algo
+// 1 create an img element
+// 2 set source and add it to our page
+// 3 add eventListener function after load and inside callback function call our another callback from func argument 'onImageLoaded'
+// 4 add eventListener in case of 'error' 
+
+const addImage = (imgSrc, callback) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'Photo');
+  imgElem.src = imgSrc;
+  const containerElem = document.querySelector('.page');
+  containerElem.append(imgElem);
+
+  const onImageLoaded = () => {
+    const { width, height } = imgElem;
+    callback(null, { width, height });
+  };
+
+  imgElem.addEventListener('load', onImageLoaded);
+
+  imgElem.addEventListener('error', () => callback('This is a mistake'));
+};
+
+// callack function
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+// examples
+addImage(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+  onImageLoaded,
+);
