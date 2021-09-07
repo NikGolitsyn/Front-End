24d074a57012b96408fa07acb4fbbda218dccfb7
Nikita <myk.holitsyn@gmail.com>
2021-09-07 08:59:40 +0300

    all

--- JS-full/lesson29/task1/index.js
+++ JS-full/lesson29/task1/index.js
@@ -9,14 +9,15 @@
 
 export const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement('img');
-  imgElem.setAttribute('alt', 'Photo');
+  imgElem.setAttribute('alt', 'My Photo');
   imgElem.src = imgSrc;
   const containerElem = document.querySelector('.page');
-  containerElem.append(imgElem);
 
   const onImageLoaded = () => {
     callback(null, imgElem);
   };
+
+  containerElem.addEventListener('load', containerElem.append(imgElem));
 
   imgElem.addEventListener('load', onImageLoaded);
 
@@ -24,7 +25,7 @@ export const addImage = (imgSrc, callback) => {
 };
 
 // callack function
-export const onImageLoaded = (error, imgElem) => {
+const onImageLoaded = (error, imgElem) => {
   if (error) {
     console.log(error);
     return;
@@ -37,10 +38,10 @@ export const onImageLoaded = (error, imgElem) => {
 };
 
 // examples
-addImage(
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  onImageLoaded,
-);
+// addImage(
+//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
+//   onImageLoaded,
+// );
 // addImage(
 //   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596',
 //   onImageLoaded,
