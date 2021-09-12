df7c307265ff4f31fe36363c8c3939f4ab81f315
NikGolitsyn <78685778+NikGolitsyn@users.noreply.github.com>
2021-09-13 00:52:30 +0300

    Update index.js

--- JS-full/test/task1/src/scripts/index.js
+++ JS-full/test/task1/src/scripts/index.js
@@ -1,6 +1,5 @@
 import { initTodoListHandler } from './todoList.js';
 import { renderTasks } from './render.js';
-// import { getTasksList } from './tasksGateway.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   renderTasks();
