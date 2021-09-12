53ced8d55b89e7d4fe4207b1443376d27ac35aa7
NikGolitsyn <78685778+NikGolitsyn@users.noreply.github.com>
2021-09-13 00:55:15 +0300

    Update createTask.js

--- JS-full/test/task1/src/scripts/createTask.js
+++ JS-full/test/task1/src/scripts/createTask.js
@@ -1,9 +1,6 @@
 import { renderTasks } from './render.js';
 import { createTask } from './tasksGateway.js';
 
-// input none
-// output undefined
-
 export const createTaskHandler = () => {
   const inputElem = document.querySelector('.task-input'); 
   const text = inputElem.value; 
@@ -22,8 +19,3 @@ export const createTaskHandler = () => {
     .then(() => renderTasks());
 };
 
-// 1. Prepare data
-// 2. Write data to DB
-// 3. Read new data from server
-// 4. Save new data to front-end storage
-// 5. Update UI based on new data
