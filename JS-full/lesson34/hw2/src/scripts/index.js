a014510f1f79fbfd273f7cd41db8b09b4578f332
NikGolitsyn <78685778+NikGolitsyn@users.noreply.github.com>
2021-09-14 13:40:50 +0300

    Update index.js

--- JS-full/lesson34/hw2/src/scripts/index.js
+++ JS-full/lesson34/hw2/src/scripts/index.js
@@ -6,8 +6,8 @@ import { submitBtnHandler } from './submitBtnHandler.js';
 // 1 disable submit button unless even one input is true
 // 2 add EventListener for submit button
 // 3 post request to server
-// 4 alert server response
-// 5 clear inputs values of form after successful respond
+// 4 alert server respond
+// 5 clear inputs values of form, after successful respond
 // 6 catch error ('Failed to create user') add this to element '.error-text'
 
 loginForm.addEventListener('input', formChangeHandler, false);
